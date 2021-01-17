d008798be3311ca46f210d908a3e6a81bbe1426f
Avinash Patel <avinash.patel@gaiansolutions.com>
2021-01-17 18:47:24 +0530

    refactor

--- src/main/java/in/auth_oauth2/config/WebSecurityConfig.java
+++ src/main/java/in/auth_oauth2/config/WebSecurityConfig.java
@@ -52,8 +52,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().anyRequest().authenticated().and().sessionManagement()
-				.sessionCreationPolicy(SessionCreationPolicy.NEVER);
+//		http.authorizeRequests().anyRequest().authenticated().and().sessionManagement()
+//				.sessionCreationPolicy(SessionCreationPolicy.NEVER);
+
+		http.authorizeRequests().anyRequest().permitAll();
 	}
 
 	@SuppressWarnings("deprecation")
